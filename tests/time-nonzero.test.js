58c0278220d9a893abbedb32166d1a1d83100bdc
ssddddddddddddddddddd <82411477+ssddddddddddddddddddd@users.noreply.github.com>
2025-05-14 02:26:14 +0300

    Update time-nonzero.test.js

--- tests/time-nonzero.test.js
+++ tests/time-nonzero.test.js
@@ -3,7 +3,7 @@ async function fn(){
   setTimeout(()=>{
   let a = await fetch("http://127.0.0.1:3000/time");
   let a_json = await a.json();
-  if(a_json.time !== 0){
+  if(a_json.time === 0){
     throw new Error('Error');
   }
   },3000)
