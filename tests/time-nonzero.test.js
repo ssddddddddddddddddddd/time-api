2db3e827560e33bb55c2ad57cd9d729fb271345b
ssddddddddddddddddddd <82411477+ssddddddddddddddddddd@users.noreply.github.com>
2025-05-14 02:28:42 +0300

    Update time-nonzero.test.js

--- tests/time-nonzero.test.js
+++ tests/time-nonzero.test.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 async function fn(){
-  setTimeout(await()=>{
+  setTimeout(async()=>{
   let a = await fetch("http://127.0.0.1:3000/time");
   let a_json = await a.json();
   if(a_json.time === 0){
