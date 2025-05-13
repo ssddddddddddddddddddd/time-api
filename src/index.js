6c313b9832a60cd054ab23275c38937f08e21de1
ssddddddddddddddddddd <you@example.com>
2025-05-14 02:54:59 +0300

    2 версия сервера

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+let a = 0;
 app.get('/time', (req, res) => {
   res.json({ time: Math.floor(Date.now() / 1000) });
+});
+app.get('/count', (req, res) => {
+  res.json({ count: a });
+  a++;
 });
 app.listen(3000, () => console.log('Listening on 3000'));
